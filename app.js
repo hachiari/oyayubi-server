6fee7e56b6cff1488cd35d86155b709b837d7bc2
beeant <beeant@gmail.com>
2014-12-05 01:31:06 +0700

    fix contentType + install mime-types

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var http = require('http');
 var GridFS = require('GridFS').GridFS;
 var gfs = new GridFS('oyayubi');
 var GridStream = require('GridFS').GridStream;
+var mime = require('mime-types');
 
 http.globalAgent.maxSockets = 1000;
 
@@ -18,7 +19,8 @@ server.ext('onRequest', function(request, reply) {
   gfs.get(query.url+query.dim, function(err, data) {
     if (err) { return reply(); }
     var stream = GridStream.createGridReadStream('oyayubi', query.url+query.dim);
-    reply(stream);
+    var contentType = mime.lookup(query.url);
+    reply(stream).type(contentType);
   });
 });
 
@@ -33,16 +35,17 @@ server.route([{
     var dims = dim.split(/x/g);
 
     var gmStream = gm(Request.get(url))
+      .quality(100)
       .resize(Number(dims[0]), Number(dims[1])+'^>')
-      .gravity('center')
+      .gravity('Center')
       .extent(dims[0], dims[1])
-      .format(function(err, format) {
-        if (err || !format) { return reply('oyayubi server: streamed payload is not an image').code(400); }
-        var contentType = 'image/'+format;
-        var w = GridStream.createGridWriteStream('oyayubi', query.url+query.dim, 'w', {conten_type: contentType});
-        gmStream.pipe(w);
-        reply(gmStream).type(contentType);
-      }).stream();
+      .autoOrient()
+      .stream();
+    var contentType = mime.lookup(url);
+    if (!contentType) { return reply('oyayubi server: streamed payload is not an image').code(400); }
+    var w = GridStream.createGridWriteStream('oyayubi', query.url+query.dim, 'w', {content_type: contentType});
+    gmStream.pipe(w);
+    reply(gmStream).type(contentType);
   }
 }, {
   method: 'POST',
@@ -61,12 +64,15 @@ server.route([{
       var payload = request.payload;
       if (!payload || !Object.keys(payload).length) { return reply('oyayubi server: image streaming payload required').code(400); }
       var gmStream = gm(payload)
+        .quality(100)
         .resize(Number(dims[0]), Number(dims[1])+'^>')
-        .gravity('center')
+        .gravity('Center')
         .extent(dims[0], dims[1])
+        .autoOrient()
         .format(function(err, format) {
           if (err || !format) { return reply('oyayubi server: streamed payload is not an image').code(400); }
-          var contentType = 'image/'+format;
+          if (format === 'jpeg') { format = 'jpg'; }
+          var contentType = 'image/'+format.toLowerCase();
           reply(gmStream).type(contentType);
         }).stream();
     }
